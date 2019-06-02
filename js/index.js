fbb272bf6abd085286b0a4539150b38c20e962de
Aleksandr <pertik@list.ru>
2019-06-02 19:34:52 +0300

    fix map

--- js/index.js
+++ js/index.js
@@ -3,83 +3,40 @@ document.addEventListener('DOMContentLoaded', function() {
   var modal = document.querySelector('#modal');
   var modalClose = modal.querySelector('.modal__close');
 
-  fbBtn.addEventListener('click', function() {
-    modal.classList.remove('modal--hide');
+  fbBtn.addEventListener('click', function(e) {
+    e.preventDefault();
     modal.classList.add('modal--show');
   });
 
-  modalClose.addEventListener('click', function() {
+  modalClose.addEventListener('click', function(e) {
+    e.preventDefault();
     modal.classList.remove('modal--show');
-    modal.classList.add('modal--hide');
   });
 
-  initMap();
+  ymaps.ready(init);
+
+  function init(){    
+    var myMap = new ymaps.Map("map", {
+      center: [59.939099, 30.321523],
+      zoom: 17,
+    });
+
+    var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+      hintContent: 'NЁRDS DESIGN STUDIO',
+      balloonContent: '191186, Санкт-Петербург, ул. Б. Конюшенная, д. 19/8'
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/pictures/map-marker.png',
+      iconImageSize: [231, 190],
+      iconImageOffset: [-40, -220]
+    });
+    
+    myMap.behaviors.disable(['drag', 'scrollZoom']);
+
+    myMap.controls
+      .remove('default')
+      .remove('rulerControl');
+      
+    myMap.geoObjects.add(myPlacemark);
+  }
 });
-
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 59.939044, lng: 30.3208509},
-    zoom: 17,
-    disableDefaultUI: true,
-    styles: [
-      {
-        'featureType': 'administrative',
-        'elementType': 'labels.text.fill',
-        'stylers': [{'color': '#444444'}],
-      },
-      {
-        'featureType': 'landscape',
-        'elementType': 'all',
-        'stylers': [{'color': '#f2f2f2'}],
-      },
-      {
-        'featureType': 'poi',
-        'elementType': 'all',
-        'stylers': [{'visibility': 'off'}],
-      },
-      {
-        'featureType': 'road',
-        'elementType': 'all',
-        'stylers': [{'saturation': -100},{'lightness': 45}],
-      },
-      {
-        'featureType': 'road.highway',
-        'elementType': 'all',
-        'stylers': [{'visibility': 'simplified'}],
-      },
-      {
-        'featureType': 'road.highway',
-        'elementType': 'geometry.fill',
-        'stylers': [{'color': '#d6d6d6'}],
-      },
-      {
-        'featureType': 'road.arterial',
-        'elementType': 'labels.icon',
-        'stylers': [{'visibility': 'off'}],
-      },
-      {
-        'featureType': 'road.local',
-        'elementType': 'geometry.fill',
-        'stylers': [{'color': '#e6e6e6'}],
-      },
-      {
-        'featureType': 'transit',
-        'elementType': 'all',
-        'stylers': [{'visibility': 'off'}],
-      },
-      {
-        'featureType': 'water',
-        'elementType': 'all',
-        'stylers': [{'color': '#86a77a'},{'visibility': 'on'}],
-      },
-    ],
-  });
-
-  var marker = new google.maps.Marker({
-    position: {lat: 59.938722, lng: 30.323050},
-    map: map,
-    title: '191186, Санкт-Петербург, ул. Б. Конюшенная, д. 19/8',
-    icon: '../img/pictures/map-marker.png',
-    // scaledSize: new google.maps.Size(25, 25),
-  });
-}
